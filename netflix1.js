c9f1c47aff88caa7eb56e1ee5b4d3718f7fdc614
sanjaysabu1132000 <sanjaysabu7777@gmail.com>
2023-11-28 11:09:49 +0530

    js file added

---
+++ netflix1.js
@@ -0,0 +1,21 @@
+
+function handle() {
+    var para = document.getElementById('demo');
+    para.innerHTML='Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.';
+}
+
+function handle1() {
+    var para = document.getElementById('demo1');
+    para.innerHTML='Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.';
+}
+
+function handle2() {
+    var para = document.getElementById('demo2');
+    para.innerHTML='Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.';
+}
+
+function handle3() {
+    var para = document.getElementById('demo3');
+    para.innerHTML='Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.';
+}
+
